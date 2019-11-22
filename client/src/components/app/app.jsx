20685dbe5e489f95821810165467776826e2dbb6
conilastra <cflastra@uc.cl>
2019-11-22 17:52:53 -0300

    total count visualization

--- client/src/components/app/app.jsx
+++ client/src/components/app/app.jsx
@@ -2,12 +2,20 @@ import React from 'react';
 import './app.css';
 import injectContext, { Consumer } from '../../store/appContext';
 import CounterHolder from '../counterHolder/counterHolder';
+import Header from '../header/header';
 
 const App = () => {
 	return (
 		<Consumer>
 			{({ store, actions }) => {
-				return <CounterHolder items={store.counters} actions={actions} />;
+				const { counters } = store;
+				const total = counters.map(c => c.count).reduce((acc, val) => acc + val);
+
+				return (
+				<>
+				<Header total={total} />
+				<CounterHolder items={counters} actions={actions} />
+				</>)
 			}}
 		</Consumer>
 	);
---
+++ client/src/components/header/header.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Header = ({ total }) => {
+	return <header>TOTAL: {total}</header>;
+};
+
+export default Header;
